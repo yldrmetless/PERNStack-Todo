5b963ac63dcb551590d7ed03a0c6203bb227ea43
Mete Yıldırım <metehan480@gmail.com>
2024-05-20 02:14:32 +0300

    updated new
    updated new

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 //get all todos
 app.get("/todos", async (req, res) => {
   try {
-    const allTodos = await pool.query("SELECT* FROM todo");
+    const allTodos = await pool.query("SELECT * FROM todo");
 
     res.json(allTodos.rows);
   } catch (err) {
